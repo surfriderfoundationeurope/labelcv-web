51b9a6e30327fe3a07e9a05259c314c2de58608a
Adrien Quillet <adrienquillet@gmail.com>
2020-05-29 02:52:37 +0200

    Checkboxes and radio are reseted on validate or skip.

--- src/components/global/Navbar.vue
+++ src/components/global/Navbar.vue
@@ -19,17 +19,16 @@
         </b-nav-item>
       </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
+      <!-- Right aligned nav avatar -->
+      <!-- <b-navbar-nav class="ml-auto">
         <b-nav-item-dropdown right no-caret>
-          <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
             <b-avatar />
           </template>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
-      </b-navbar-nav>
+      </b-navbar-nav> -->
     </b-collapse>
   </b-navbar>
 </template>
