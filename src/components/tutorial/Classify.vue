fc623e8e46044a23f6810449eeff7b650e98e001
Adrien Quillet <adrienquillet@gmail.com>
2020-12-11 00:11:26 +0100

    Changes according to Antoine's feedback

--- src/components/tutorial/Classify.vue
+++ src/components/tutorial/Classify.vue
@@ -1,132 +1,142 @@
 <template>
-    <div>
-        <div class="illustration"></div>
-        <div class="explanation">
-            <h1>Classify pictures</h1>
-            <p>
-                Go to the “Classify” section to see uploaded pictures (all pictures are mixed. If you uploaded, you won’t only see yours). 
-            </p>
-            <p>For each picture:</p>
-            <ol class="box">
-                <li>Click in the right panel to indicate where the picture was taken (this enables the data to be used for research) 
-                    <div class="annotation-group">
-                        <b-form-group>
-                            <b-form-radio-group
-                                :options="state.contextEnvClasses.options"
-                                name="env-buttons"
-                                buttons
-                                button-variant="dark"
-                                size="sm"
-                                />
-                            </b-form-group>
-                    </div>
-                </li>
-                <li>Indicate the viewpoint. Is the picture taken from above or sideway?
-                    <div class="annotation-group">
-                        <b-form-group>
-                            <b-form-radio-group
-                                :options="state.contextPovClasses.options"
-                                name="pov-buttons"
-                                buttons
-                                button-variant="dark"
-                                size="sm"
-                            />
-                        </b-form-group>
-                    </div>
-                </li>
-                <li>Specify the quality of the picture. Choose bad if the picture is blurry or pixelized
-                    <div class="annotation-group">
-                        <b-form-group>
-                            <b-form-radio-group
-                                :options="state.contextQualityClasses.options"
-                                buttons
-                                name="quality-buttons"
-                                button-variant="dark"
-                                size="sm"
-                            />
-                        </b-form-group>
-                    </div>
-                </li>
-            </ol>
+  <div>
+    <div class="illustration"></div>
+    <div class="explanation">
+      <p class="pre-title">Step {{ this.$props.step }}</p>
+      <h1>Classify pictures</h1>
+      <p>
+        In the <strong>Annotate</strong> screen, a first picture will appear. We
+        need you to classify the picture. To do that, go to the bottom right
+        panel.
+      </p>
+      <p>For each picture:</p>
+      <ol class="box">
+        <li>
+          First, indicate where the picture was taken.
+          <div class="annotation-group">
+            <b-form-group>
+              <b-form-radio-group
+                :options="state.contextEnvClasses.options"
+                name="env-buttons"
+                buttons
+                button-variant="dark"
+                size="sm"
+              />
+            </b-form-group>
+          </div>
+        </li>
+        <li>
+          Then, precise the viewpoint. Is the picture taken from above or
+          sideway?
+          <div class="annotation-group">
+            <b-form-group>
+              <b-form-radio-group
+                :options="state.contextPovClasses.options"
+                name="pov-buttons"
+                buttons
+                button-variant="dark"
+                size="sm"
+              />
+            </b-form-group>
+          </div>
+        </li>
+        <li>
+          Last, specify the quality of the picture. Choose bad if the picture is
+          blurry or pixelized.
+          <div class="annotation-group">
+            <b-form-group>
+              <b-form-radio-group
+                :options="state.contextQualityClasses.options"
+                buttons
+                name="quality-buttons"
+                button-variant="dark"
+                size="sm"
+              />
+            </b-form-group>
+          </div>
+        </li>
+      </ol>
 
-            <b-button
-            class="btn-next-step"
-            variant="secondary"
-            @click="$emit('previousstep')"
-            >
-            Back 
-            </b-button>
+      <b-button
+        class="btn-next-step"
+        variant="secondary"
+        @click="$emit('previousstep')"
+      >
+        Back
+      </b-button>
 
-            <b-button
-            class="btn-next-step"
-            variant="primary"
-            @click="$emit('nextstep')"
-            >
-            Next >
-            </b-button>
-        </div>
+      <b-button
+        class="btn-next-step"
+        variant="primary"
+        @click="$emit('nextstep')"
+      >
+        Next >
+      </b-button>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import Component from 'vue-class-component';
-    import { getModule } from 'vuex-module-decorators';
-    import AnnotationStore from '@/store/store.annotation';
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { getModule } from 'vuex-module-decorators';
+import AnnotationStore from '@/store/store.annotation';
 
-    @Component({})
-    export default class Classify extends Vue {
-        private readonly state: AnnotationStore = getModule(AnnotationStore);
-
-    }
+@Component({
+  props: ['step'],
+})
+export default class Classify extends Vue {
+  private readonly state: AnnotationStore = getModule(AnnotationStore);
+}
 </script>
 
 <style scoped>
-     .explanation {
-        width:45%;
-        margin-left: auto;
-    }
+.explanation {
+  width: 45%;
+  margin-left: auto;
+  padding-bottom: 5rem;
+}
 
-   
-    .illustration {
-        width:50%;
-        background-image: url("/images/tutorial/tuto-classify-scene.png"); 
-        background-repeat: no-repeat;
-        height: 100%;
-        background-size:cover;
-        background-position: center;
-        position:fixed;
-        top:0;
-        left:0;
-        display:flex;
-        justify-content: center;
-        align-items: center;
-    }
+.illustration {
+  width: 50%;
+  background-image: url("/images/tutorial/tuto-classify-scene.jpg");
+  background-repeat: no-repeat;
+  height: 100%;
+  background-size: cover;
+  background-position: right;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
 
+.illustration img {
+  margin-top: auto;
+  margin-bottom: auto;
+}
 
-    .illustration img {
-        width:60%;
-        margin-top:auto;
-        margin-bottom:auto;
-    }
+.pre-title {
+  font-weight: bold;
+  margin-bottom: 0;
+  color: #0073be;
+}
 
-    .box {
-        margin: 2rem 0;
-        padding:2rem 3rem;
-        border: 1px solid white;
-        border-radius: 0.3rem;
-    }
+.box {
+  margin: 2rem 0;
+  padding: 2rem 3rem;
+  border: 1px solid white;
+  border-radius: 0.3rem;
+}
 
-    .box li {
-        margin-top: 0.6rem;
-    }
+.box li {
+  margin-top: 0.6rem;
+}
 
-    .annotation-group {
-        margin: 0.5em 0;
-    }
+.annotation-group {
+  margin: 0.5em 0;
+}
 
-    .annotation-group p {
-        font-weight: bold;
-        margin: 0;
-    }
+.annotation-group p {
+  font-weight: bold;
+  margin: 0;
+}
 </style>
--- src/components/tutorial/Draw.vue
+++ src/components/tutorial/Draw.vue
@@ -1,91 +1,180 @@
 <template>
-    <div>
-        <div class="illustration"></div>
-        <div class="explanation">
-            <h1>Draw bounding boxes</h1>
-            <p>And now, the most important part: we need you to draw bounding boxes on each litter item.</p>
-            <p>
-                It teaches the Artificial Intelligence what’s litter and what’s not! 
-                Use the magnifying glass in the top right corner to draw precisely. 
-            </p>    
-            <p>Here are few tips to draw bounding boxes</p>
-            <ol class="box">
-                <li>Encompass as close as possible the litter item in the box</li>
-                <li>Capture the full object</li>
-                <li>Exclude from the box the reflexion of the trash on water</li>
-            </ol>
-            <p>More tips</p>
-            <ul class="box">
-                <li>When unsure whether it’s one or two objects: Capture just one</li>
-                <li>When several very small objects are nearby: Capture them all together</li>
-                <li>When unsure if this is a trash: Do not label it</li>
-                <li>It's OK to label pictures featuring paddles or kayak parts; however, paddles & kayak parts should not be labelled as litter items.</li>
-            </ul>
-            <b-button
-            class="btn-next-step"
-            variant="secondary"            
-            @click="$emit('previousstep')"
-            >
-            Back 
-            </b-button>
-
-            <b-button
-            class="btn-next-step"
-            variant="primary"
-            @click="$emit('nextstep')"
-            >
-            Next >
-            </b-button>
-        </div>    
+  <div>
+    <div class="illustration">
+      <div>
+        <div class="img-wrapper">
+          <img
+            src="/images/tutorial/tuto-wrong-1.jpg"
+            alt="Example of a wrong bounding box"
+          />
+        </div>
+        <div class="img-wrapper">
+          <img
+            src="/images/tutorial/tuto-right-1.jpg"
+            alt="Example of a right bounding box"
+          />
+        </div>
+      </div>
+      <div>
+        <div class="img-wrapper">
+          <img
+            src="/images/tutorial/tuto-wrong-2.jpg"
+            alt="Example of a wrong bounding box"
+          />
+        </div>
+        <div class="img-wrapper">
+          <img
+            src="/images/tutorial/tuto-right-2.jpg"
+            alt="Example of a right bounding box"
+          />
+        </div>
+      </div>
+      <div>
+        <div class="img-wrapper">
+          <img
+            src="/images/tutorial/tuto-wrong-3.jpg"
+            alt="Example of a wrong bounding box"
+          />
+        </div>
+        <div class="img-wrapper">
+          <img
+            src="/images/tutorial/tuto-right-3.jpg"
+            alt="Example of a right bounding box"
+          />
+        </div>
+      </div>
     </div>
+    <div class="explanation">
+      <p class="pre-title">Step {{ this.$props.step }}</p>
+      <h1>Draw bounding boxes</h1>
+      <p>
+        And now, the most important part: we need you to draw bounding boxes on
+        each litter item.
+      </p>
+      <p>
+        It teaches the Artificial Intelligence what’s litter and what’s not! Use
+        the magnifying glass in the top right corner to draw precisely.
+      </p>
+      <p>Here are few tips to draw bounding boxes</p>
+      <ol class="box">
+        <li>Encompass as close as possible the litter item in the box</li>
+        <li>Capture the full object</li>
+        <li>Exclude from the box the reflexion of the trash on water</li>
+      </ol>
+      <p>More tips</p>
+      <ul class="box">
+        <li>When unsure whether it’s one or two objects: capture just one</li>
+        <li>
+          When several very small objects are nearby: capture them all together
+        </li>
+        <li>When unsure if this is a trash: Do not label it</li>
+        <li>
+          It's OK to label pictures featuring paddles or kayak parts; however,
+          paddles & kayak parts should not be labelled as litter items.
+        </li>
+      </ul>
+      <b-button
+        class="btn-next-step"
+        variant="secondary"
+        @click="$emit('previousstep')"
+      >
+        Back
+      </b-button>
+
+      <b-button
+        class="btn-next-step"
+        variant="primary"
+        @click="$emit('nextstep')"
+      >
+        Next >
+      </b-button>
+    </div>
+  </div>
 </template>
 
-
 <script lang="ts">
-    import Vue from 'vue';
-    import Component from 'vue-class-component';
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
-    @Component({})
-    export default class Draw extends Vue {}
+@Component({
+  props: ['step'],
+})
+export default class Draw extends Vue {}
 </script>
 
 <style scoped>
- .explanation {
-        width:45%;
-        margin-left: auto;
-    }
+.explanation {
+  width: 45%;
+  margin-left: auto;
+  padding-bottom: 5rem;
+}
 
-   
-    .illustration {
-        width:50%;
-        background-image: url("/images/tutorial/placeholder-bottle.png"); 
-        background-repeat: no-repeat;
-        height: 100%;
-        background-size:cover;
-        background-position: center;
-        position:fixed;
-        top:0;
-        left:0;
-        display:flex;
-        justify-content: center;
-        align-items: center;
-    }
+.illustration {
+  width: 50%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.img-wrapper {
+  position: relative;
+  display: inline;
+  height: auto;
+  width: 100%;
+}
+.icon {
+  position: absolute;
+  height: 1.5rem;
+  width: 1.5rem;
+  background-color: yellow;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.icon p {
+  margin: 0;
+  font-weight: bold;
+}
 
-    .illustration img {
-        width:60%;
-        margin-top:auto;
-        margin-bottom:auto;
-    }
+.wrong {
+  background-color: #cf2b29;
+}
 
-    .box {
-        margin: 2rem 0;
-        padding:2rem 3rem;
-        border: 1px solid white;
-        border-radius: 0.3rem;
-    }
+.wrong p {
+  font-size: 1.5rem;
+  transform: rotate(45deg);
+}
 
-    .box li {
-        margin-top: 0.6rem;
-    }
+.right {
+  background-color: #0073be;
+}
+
+.right p {
+  font-size: 0.9rem;
+}
+
+.illustration img {
+  width: 40%;
+  margin-bottom: 5rem;
+  margin-left: 2rem;
+}
+
+.pre-title {
+  font-weight: bold;
+  margin-bottom: 0;
+  color: #0073be;
+}
+.box {
+  margin: 2rem 0;
+  padding: 2rem 3rem;
+  border: 1px solid white;
+  border-radius: 0.3rem;
+}
+
+.box li {
+  margin-top: 0.6rem;
+}
 </style>
