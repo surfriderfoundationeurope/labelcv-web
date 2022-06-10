15fda459a9647f8bc39886ba2bc9e14d0a582440
JR <jeanremy@users.noreply.github.com>
2022-06-10 10:30:01 +0200

    Ajout de l'upload de photos (#129)
    * Add upload route to router
    
    * :art: Add Upload page and form
    
    * :art: Add multiple to files upload
    
    * :label: Fix TS
    
    * :art: Redirect to home after logout
    
    * :speech_bubble: Change labels on upload page
    
    * :art: Implements api endpoint
    
    * Update api endpoint

---
+++ src/views/Upload.vue
@@ -0,0 +1,29 @@
+<template>
+    <UploadForm />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import UploadForm from "../components/upload/UploadForm.vue";
+import { mapGetters } from "vuex";
+
+@Component({
+    computed: {
+        ...mapGetters(["isLoggedIn"])
+    },
+    components: {
+        UploadForm
+    }
+})
+export default class Upload extends Vue {
+    loggedIn = this.$store.getters.isLoggedIn;
+    private mounted(): void {
+        if (!this.loggedIn) {
+            this.$router.push("login");
+        }
+    }
+}
+</script>
+
+<style scoped></style>
